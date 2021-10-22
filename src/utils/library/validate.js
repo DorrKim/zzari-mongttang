f42e0e239cacd76a26caade20cddc6a555b9a789
Alang Kim <rlaangh77@naver.com>
2021-10-23 00:36:44 +0900

    feat: 이메일, 이름, 비밀번호, 비밀번호 확인 검증 로직 추가

---
+++ src/utils/library/validate.js
@@ -0,0 +1,30 @@
+const MIN_FULLNAME_LENGTH = 2;
+const MAX_FULLNAME_LENGTH = 10;
+const MIN_PASSWORD_LENGTH = 6;
+const MAX_PASSWORD_LENGTH = 18;
+
+export const validateEmail = email => {
+  const regex = /\S+@\S+\.\S+/;
+  
+  return regex.test(email);
+};
+
+export const validateFullName = fullName => {
+  if (MIN_FULLNAME_LENGTH <= fullName.length 
+    && fullName.length <= MAX_FULLNAME_LENGTH) {
+    return true;
+  }
+};
+
+export const validatePassword = password => {
+  if (MIN_PASSWORD_LENGTH <= password.length 
+    && password.length <= MAX_PASSWORD_LENGTH) {
+    return true;
+  }
+};
+
+export const validateVerifyPassword = (password1, password2) => {
+  if (password1 === password2) {
+    return true;
+  }
+};
