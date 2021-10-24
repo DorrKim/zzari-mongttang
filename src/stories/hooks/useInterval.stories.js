57c2f5cbd8028ffc4fc7c9e438d034897ba18631
Kim hyunseok <dorrdorr9311@gmail.com>
2021-10-24 16:05:11 +0900

    feat: useInterval.stories.js 스토리파일 추가 및 스토리작성

---
+++ src/stories/hooks/useInterval.stories.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import useInterval from '@/hooks/useInterval';
+
+export default {
+  title: 'Hook/useInterval'
+};
+
+export const Default = () => {
+  const [array, setArray] = useState([]);
+  const clear = useInterval(() => {
+    setArray([...array, '추가됨 ']);
+  }, 1000);
+  
+  return (
+    <>
+      <div> useIntervalFn 테스트</div>
+      <div> 1초마다 추가됩니다.</div>
+      <div> {array} </div>
+      <button onClick={clear}>중지</button>
+    </>
+  );
+};
