2169b19de43adb5c8b66d60ade46d87b71fb9fb3
young-d <dodoga096@gmail.com>
2021-10-25 05:10:26 +0900

    fix: base url 설정

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,6 +2,8 @@ import useAsyncFn from '@hooks/useAsyncFn';
 import axios from 'axios';
 
 
+axios.defaults.baseURL = 'http://13.209.30.200:5001';
+
 const useAxios = (url, options = {}) => useAsyncFn(
   async() => await axios({
     url,
