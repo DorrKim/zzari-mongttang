c1d6aec43758753858afa67fb01128f014feecbc
Alang Kim <rlaangh77@naver.com>
2021-10-24 19:30:08 +0900

    fix: 코드 최적화 및 onSignUp, onCancel 통한 props를 통해 이벤트로직 수행 변경

--- src/domains/SignUpForm/index.js
+++ src/domains/SignUpForm/index.js
@@ -1,163 +1,164 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { useHistory } from 'react-router';
+// import { useHistory } from 'react-router';
+import PropTypes from 'prop-types';
 
 import Button from '@base/Button';
 import { validateEmail, validateFullName, validatePassword, validateVerifyPassword } from '@library/validate';
-import { loginAPI } from '@api/loginAPI';
 import FormInput from '@components/FormInput';
 import Flex from '@/components/base/Flex';
 
-const SignUpForm = () => {
-  const [email, setEmail] = useState({
-    value: '',
+const SignUpForm = ({ onSignUp, onCancel }) => {
+  const [email, setEmail] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [password, setPassword] = useState('');
+  const [verifyPassword, setVerifyPassword] = useState('');
+  const [validateForm, setValidateForm] = useState({
     isEmail: false,
-    inUse: false
-  });
-  const [fullName, setFullName] = useState({
-    value: '',
-    isFullName: false
-  });
-  const [password, setPassword] = useState({
-    value: '',
-    isPassword: false
-  });
-  const [verifyPassword, setVerifyPassword] = useState({
-    value: '',
+    isEmailInUse: false,
+    isFullName: false,
+    isPassword: false,
     isVerifyPassword: false
   });
   const [isSignUpable, setIsSignUpable] = useState(false);
-  const history = useHistory();
 
   useEffect(() => {
-    if (email.isEmail 
-      && fullName.isFullName 
-      && password.isPassword 
-      && verifyPassword.isVerifyPassword
+    if (
+      validateForm.isEmail 
+      && validateForm.isFullName 
+      && validateForm.isPassword 
+      && validateForm.isVerifyPassword
     ) {
       setIsSignUpable(true);
     } else {
       setIsSignUpable(false);
     }
-  }, [email.isEmail, fullName.isFullName, password.isPassword, verifyPassword.isVerifyPassword]);
+  }, [validateForm]);
 
   useEffect(() => {
-    setEmail(email => validateEmail(email.value)
-      ? { ...email,
-        isEmail: true } 
-      : { ...email,
-        isEmail: false });
-    setFullName(fullName => validateFullName(fullName.value)
-      ? { ...fullName,
-        isFullName: true }
-      : { ...fullName,
-        isFullName: false });
-    setPassword(password => validatePassword(password.value)
-      ? { ...password,
-        isPassword: true }
-      : { ...password,
-        isPassword: false });
-    setVerifyPassword(verifyPassword => validateVerifyPassword(password.value, verifyPassword.value)
-      ? { ...verifyPassword,
-        isVerifyPassword: true }
-      : { ...verifyPassword,
-        isVerifyPassword: false });
-  }, [email.value, fullName.value, password.value, verifyPassword.value]);
+    setValidateForm(validateForm => ({
+      ...validateForm,
+      isPassword: validatePassword(password),
+      isVerifyPassword: validateVerifyPassword(password, verifyPassword)
+    }));
+  }, [password, verifyPassword]);
+
 
   const handleEmailChange = useCallback(value => {
-    setEmail({ ...email,
-      value,
-      inUse: false });
-  }, [email]);
+    setEmail(value);
+    setValidateForm(validateForm => ({ ...validateForm,
+      isEmail: validateEmail(value) }));
+  }, []);
 
   const handleFullNameChange = useCallback(value => {
-    setFullName({ ...fullName,
-      value });
-  }, [fullName]);
+    setFullName(value);
+    setValidateForm(validateForm => ({ ...validateForm,
+      isFullName: validateFullName(value) }));
+  }, []);
 
   const handlePasswordChange = useCallback(value => {
-    setPassword({ ...password,
-      value });
-  }, [password]);
+    setPassword(value);
+    setValidateForm(validateForm => ({ ...validateForm,
+      isPassword: validatePassword(value) }));
+  }, []);
 
   const handleVerifyPasswordChange = useCallback(value => {
-    setVerifyPassword({ ...verifyPassword,
-      value });
-  }, [verifyPassword]);
+    setVerifyPassword(value);
+  }, []);
 
-  const handleSignUp = useCallback(async e => {
+  const handleSignUpClick = useCallback(async e => {
     e.preventDefault();
     if (!isSignUpable){
       return;
     }
     setIsSignUpable(false);
-    const data = await loginAPI.postSignUp({
-      email: email.value,
-      fullName: fullName.value,
-      password: password.value
+    const error = onSignUp && await onSignUp({
+      email,
+      fullName,
+      password
     });
 
-    if (!data) {
-      setEmail({ ...email,
-        inUse: true });
+    if (error) {
+      setValidateForm({ ...validateForm,
+        isEmailInUse: true });
+      setIsSignUpable(false);
+    } else {
       setIsSignUpable(true);
-      
-      return;
     }
-
-    await loginAPI.postLogin({
-      email: email.value,
-      password: password.value
-    });
-
-    history.push('/');
+    
   });
 
-  const handleCancel = useCallback(() => {
-    if (window.confirm('회원가입 페이지에서 나가시겠습니까? 입력한 정보는 저장되지 않습니다.')){
-      history.push('/');
-    }
-  });
+  const handleCancelClick = useCallback(() => {
+    onCancel && onCancel();
+  }, [onCancel]);
+
+  const { isEmail, isEmailInUse, isFullName, isPassword, isVerifyPassword } = validateForm;
   
   return (
     <Flex justifyContent='center'>
-      <form>
+      <form onSubmit={handleSignUpClick} >
         <FormInput 
           onChange={handleEmailChange} 
-          value={email.value} 
+          value={email} 
           placeholder='이메일' 
-          errorMessage={email.isEmail 
-            ? email.inUse 
+          errorMessage={isEmail 
+            ? isEmailInUse 
               ? '이미 사용중인 이메일입니다'
               : ''
             : '이메일 형식에 맞추어주세요.'} 
         />
         <FormInput
           onChange={handleFullNameChange}
-          value={fullName.value}
+          value={fullName}
           placeholder='닉네임'
-          errorMessage={fullName.isFullName ? '' : '2자이상 10자이하의 이름을 입력해주세요. '}
+          errorMessage={isFullName ? '' : '2자이상 10자이하의 이름을 입력해주세요. '}
         />
         <FormInput
           type='password'
           onChange={handlePasswordChange}
-          value={password.value}
+          value={password}
           placeholder='비밀번호'
-          errorMessage={password.isPassword ? '' : '6자이상 18자이하의 비밀번호를 입력해주세요.'}
+          errorMessage={isPassword ? '' : '6자이상 18자이하의 비밀번호를 입력해주세요.'}
         />
         <FormInput
           type='password'
           onChange={handleVerifyPasswordChange}
-          value={verifyPassword.value}
+          value={verifyPassword}
           placeholder='비밀번호 확인'
-          errorMessage={verifyPassword.value && verifyPassword.isVerifyPassword ? '' : '비밀번호가 일치하지 않습니다.'}
+          errorMessage={verifyPassword && isVerifyPassword ? '' : '비밀번호가 일치하지 않습니다.'}
         />
         <Flex justifyContent='space-between'>
-          <Button type='submit' disabled={!isSignUpable} onClick={handleSignUp} style={{ border: 'none' }} borderRadius={'4px'} backgroundColor={'#FD9F28'} width={'164px'} height={'60px'} >생성 / 수정</Button>
-          <Button onClick={handleCancel} style={{ border: 'none' }} borderRadius={'4px'} backgroundColor={'#dddd'} width={'164px'} height={'60px'} >취소</Button>
+          <Button 
+            type='submit' 
+            disabled={!isSignUpable} 
+            style={{ border: 'none' }} 
+            borderRadius={'4px'}
+            backgroundColor={'#FD9F28'} 
+            width={164} 
+            height={60}
+            hover={{ filter: 'brightness(90%)' }}
+            focus={null}
+            active={{ filter: 'brightness(80%)' }}
+          >생성 / 수정</Button>
+          <Button 
+            style={{ border: 'none' }}
+            borderRadius={'4px'} 
+            backgroundColor={'#dddd'} 
+            width={'164px'} 
+            height={'60px'}
+            hover={{ filter: 'brightness(90%)' }}
+            focus={null}
+            active={{ filter: 'brightness(80%)' }}
+            onClick={handleCancelClick} 
+          >취소</Button>
         </Flex>
       </form>
     </Flex>
   );
+};
+
+SignUpForm.propTypes = {
+  onSignUp: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired
 };
 
 export default SignUpForm;
