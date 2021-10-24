072129f465fb48639741d46d846082c3396edc24
Alang Kim <rlaangh77@naver.com>
2021-10-24 19:30:40 +0900

    fix: 코드 최적화 및 onLogin, onToSubmitPage props를 통해 이벤트로직 수행 변경

--- src/domains/LoginForm/index.js
+++ src/domains/LoginForm/index.js
@@ -1,80 +1,56 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { useHistory } from 'react-router';
+// import { useHistory } from 'react-router';
+import PropTypes from 'prop-types';
 
 import FormInput from '@/components/FormInput';
 import Flex from '@base/Flex';
 import Button from '@base/Button';
 import { validateEmail } from '@library/validate';
-import { loginAPI } from '@api/loginAPI';
 
-const LoginForm = ({ ...props }) => {
-  const [email, setEmail] = useState({
-    value: '',
-    isEmail: false,
-    inUse: false
-  });
-  const [password, setPassword] = useState({
-    value: '',
-    isPassword: true
-  });
+const LoginForm = ({ onLogin, onToSubmitPage, ...props }) => {
+  const [email, setEmail] = useState('');
+  const [isEmail, setIsEmail] = useState(false);
+  const [password, setPassword] = useState('');
   const [isLoginFailed, setisLoginFailed] = useState(false);
-  const history = useHistory();
 
   useEffect(() => {
-    setEmail(email => validateEmail(email.value)
-      ? { ...email,
-        isEmail: true } 
-      : { ...email,
-        isEmail: false });
-  }, [email.value]);
-
-  const handleEmailChange = useCallback(value => {
-    setEmail({ ...email,
-      value });
-    setisLoginFailed(false);
+    setIsEmail(validateEmail(email));
   }, [email]);
 
-  const handlePasswordChange = useCallback(value => {
-    setPassword({ ...password,
-      value });
+  const handleEmailChange = useCallback(value => {
+    setEmail(value);
     setisLoginFailed(false);
+  }, []);
 
-  }, [password]);
+  const handlePasswordChange = useCallback(value => {
+    setPassword(value);
+    setisLoginFailed(false);
+  }, []);
 
   const handleLogin = useCallback(async e => {
     e.preventDefault();
-    if (!email.isEmail) {
+    if (!isEmail) {
       return;
     }
 
-    const user = await loginAPI.postLogin({
-      email: email.value,
-      password: password.value
-    });
-
-    if (!user) {
-      setisLoginFailed(true);
+    const error = onLogin && await onLogin({ email,
+      password });
       
-      return;
-    }
-
-    history.push('/');
+    error && setisLoginFailed(true);
   });
 
-  const handleCancel = useCallback(() => {
-    if (window.confirm('로그인 페이지에서 나가시겠습니까? 입력한 정보는 저장되지 않습니다.')){
-      history.push('/');
-    }
+  const handleToSubmitPage = useCallback(() => {
+    onToSubmitPage && onToSubmitPage();
   });
 
   return (
     <Flex justifyContent='center'>
-      <form {...props}>
+      <form onSubmit={handleLogin} {...props}>
         <FormInput 
           onChange={handleEmailChange} 
-          value={email.value} 
+          value={email} 
           placeholder='이메일' 
-          errorMessage={!email.isEmail 
+          errorMessage={!isEmail 
             ? '이메일 형식에 맞추어주세요.'
             : ''
           } 
@@ -82,38 +58,53 @@ const LoginForm = ({ ...props }) => {
         <FormInput
           type='password'
           onChange={handlePasswordChange}
-          value={password.value}
+          value={password}
           placeholder='비밀번호'
-          errorMessage={isLoginFailed && email.isEmail
+          errorMessage={isLoginFailed && isEmail
             ? '로그인에 실패하였습니다. 이메일 또는 비밀번호를 확인해주세요'
             : ''}
         />
-        <Flex justifyContent='space-between'>
-          <Button 
-            type='submit' 
-            width={164} 
-            height={60}
-            backgroundColor={'#FD9F28'} 
-            borderRadius={'4px'} 
-            style={{ border: 'none' }} 
-            onClick={handleLogin}
-          > 
+        <Button 
+          type='submit'
+          disabled={!isEmail}
+          style={{ 
+            border: 'none',
+            display: 'block',
+            marginBottom: 16 }}
+          backgroundColor={'#FD9F28'} 
+          borderRadius={'4px'} 
+          width={328}
+          height={60}
+          hover={{ filter: 'brightness(90%)' }}
+          focus={null}
+          active={{ filter: 'brightness(80%)' }}
+        > 
           로그인 
-          </Button>
-          <Button 
-            width={164} 
-            height={60}
-            backgroundColor={'#dddd'}
-            borderRadius={'4px'}
-            style={{ border: 'none' }} 
-            onClick={handleCancel} 
-          >
-            취소
-          </Button>
-        </Flex>
+        </Button>
+        <Button 
+          style={{ 
+            border: 'none',
+            display: 'block' 
+          }}
+          backgroundColor={'#ffffff'}
+          borderRadius={'4px'}
+          width={328} 
+          height={60}
+          hover={{ filter: 'brightness(90%)' }}
+          focus={null}
+          active={{ filter: 'brightness(80%)' }}
+          onClick={handleToSubmitPage} 
+        >
+            회원가입
+        </Button>
       </form>
     </Flex>
   );
+};
+
+LoginForm.propTypes = {
+  onLogin: PropTypes.func,
+  onToSubmitPage: PropTypes.func
 };
 
 export default LoginForm;
