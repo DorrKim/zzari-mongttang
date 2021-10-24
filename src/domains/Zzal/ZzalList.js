7d20eeb41f680ac44f75fe635a68c669ced4ce9f
young-d <dodoga096@gmail.com>
2021-10-24 16:51:23 +0900

    fix: observer 작동 오류 해결

--- src/domains/Zzal/ZzalList.js
+++ src/domains/Zzal/ZzalList.js
@@ -1,52 +1,72 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 
 import useAsync from '@hooks/useAsync';
 import styled from '@emotion/styled';
 import Image from '@base/Image';
 
+
+let observer = null;
+const LOAD_IMG_EVENT_TYPE = 'fetch';
+
+const checkIntersect = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      observer.unobserve(entry.target);
+      entry.target.dispatchEvent(new CustomEvent(LOAD_IMG_EVENT_TYPE));
+      observer.observe(entry.target);
+    } 
+  });
+};
+
 const ZzalList = () => {
-  const [itemCount, setItemCount] = useState(0);
-  const [ref, setRef] = useState(null);
+  const [itemCount, setItemCount] = useState(9);
+  const [isInit, setIsInit] = useState(false);
+  const ref = useRef(null);
+  const fetchItem = () => setItemCount(prev => prev + 9);
+  const TEST_IMG_URL = 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/d373d836-bb5d-4bf6-8f12-baebd8aa3dca/%EC%A0%9C%EB%AA%A9%EC%9D%84_%EC%9E%85%EB%A0%A5%ED%95%B4%EC%A3%BC%EC%84%B8%EC%9A%94_-003_%281%29.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20211024%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211024T062214Z&X-Amz-Expires=86400&X-Amz-Signature=ed791d1519057291afbd5941bd826c845dfc979358c99d6a3d3575d0631603c7&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22%25EC%25A0%259C%25EB%25AA%25A9%25EC%259D%2584%2520%25EC%259E%2585%25EB%25A0%25A5%25ED%2595%25B4%25EC%25A3%25BC%25EC%2584%25B8%25EC%259A%2594_-003%2520%281%29.png%22';
+
+  useEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+
+    ref.current.addEventListener(LOAD_IMG_EVENT_TYPE, () => fetchItem());
+
+    return () => {
+      ref.current.removeEventListener(LOAD_IMG_EVENT_TYPE, () => fetchItem());
+    };
+  }, [ref]);
 
   const initialPosts = useAsync(async () => {
-    console.log('apitest.......');
-    
     return await axios  
       .get('https://jsonplaceholder.typicode.com/photos')
       .then(res => res.data);
-  }, []);
+  }, []);  
 
-
-  useEffect(() => {
-    let observer;
-    if (ref) {
-      observer = new IntersectionObserver(checkIntersect, { threshold: 1 });
-      observer.observe(ref);
+  useEffect(() => {      
+    if (!observer) {
+      observer = new IntersectionObserver(checkIntersect, { threshold: 0 });
     }
-    
-    return () => observer && observer.disconnect();
-  }, [ref]);
 
-  const fetchItems = async () => setItemCount(prev => prev + 9);
-
-  const checkIntersect = async ([entry], observer) => {
-    if (!entry.isIntersecting) {
+    if (!isInit) {
+      setIsInit(true);
+      
       return;
     }
-    observer.unobserve(entry.target);
-    await fetchItems();
-    observer.observe(entry.target);
-  };
 
-  console.log(itemCount);
-
+    if (ref.current && !initialPosts.isLoading) {
+      observer.observe(ref.current);
+    }
+  }, [isInit, initialPosts, observer]);
+  
   return (
     <StyledList>
       {(initialPosts.value || []).filter((_, idx) => idx < itemCount).map(post => (
-        <Image key={post.id} src='https://s3.us-west-2.amazonaws.com/secure.notion-static.com/3867a8b5-610b-420e-b1a1-0e7c8b19a2dd/20211019_616ed2e7aaef5.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20211022%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211022T182252Z&X-Amz-Expires=86400&X-Amz-Signature=c07bb148d5c657fbced09ebef126e34d1c19f713381fe609d38566fc20fb772c&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%2220211019_616ed2e7aaef5.gif%22' type='circle'/>
-      ))}
-      <div ref={setRef} />
+        <Image key={post.id} src={TEST_IMG_URL} type='circle' height='200px'/>
+      )
+      )}  
+      <div ref={ref}></div>
     </StyledList>
   );
 };
