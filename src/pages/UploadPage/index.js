6c68823a806710616cb1f662509dbef7f4b853d1
khj <hongjung.kim.dev@gmail.com>
2021-10-26 04:27:20 +0900

    feat: 클릭 및 드래그 앤 드랍으로 이미지 업로더 기본기능 구현

--- src/pages/UploadPage/index.js
+++ src/pages/UploadPage/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import UploaderTemplate from '@domains/UploaderTemplate';
 
 const UploadPage = () => {
   
-  return <h1> 업로드 </h1>;
+  return (
+    <UploaderTemplate
+      droppable={true}
+      width={'328px'}
+      height={'248px'}
+      type={'square'}
+    >
+    </UploaderTemplate>
+  );
 };
-
 
 export default UploadPage;
