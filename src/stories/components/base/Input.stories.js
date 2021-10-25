208860ab7c1e288b9f530466ba5f548f805e007e
Alang Kim <rlaangh77@naver.com>
2021-10-25 15:11:39 +0900

    chore: Input story 컨트롤 range로 변경

--- src/stories/components/base/Input.stories.js
+++ src/stories/components/base/Input.stories.js
@@ -5,8 +5,21 @@ export default {
   title: 'Component/Base/Input',
   component: Input,
   argTypes: {
-    width: { control: 'number' },
-    height: { control: 'number' },
+    width: { 
+      control: {
+        type: 'range',
+        min: 50,
+        max: 500,
+        step: 1 
+      }
+    },
+    height: { 
+      control: {
+        type: 'range',
+        min: 10,
+        max: 100,
+        step: 1 
+      }},
     fontSize: { control: 'number' }
   }
 };
