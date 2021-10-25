481849add23e0742ecbe1b94a5d4e308a88f2717
young-d <dodoga096@gmail.com>
2021-10-26 04:52:27 +0900

    feat: 짤아이템 컴포넌트 구현

--- src/domains/Zzal/ZzalItem.js
+++ src/domains/Zzal/ZzalItem.js
@@ -1,32 +1,58 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import Image from '@base/Image';
 import styled from '@emotion/styled';
-
+import { useHistory } from 'react-router';
+import ReactFreezeframe from 'react-freezeframe';
 
 const ZzalItem = ({
+  id,
   imageUrl = '',
   ...props
 }) => {
+  const history = useHistory();
+  const freeze = useRef();
+
+  const onToDetailPage = useCallback(() => {
+    history.push(`/zzal/${id}`);
+  });
+
   return (
-    <StyledItem style={{ alignContent: 'center' }}>
-      <Image src={imageUrl} {...props}></Image>
-    </StyledItem>
+    <ReactFreezeframe 
+      ref={freeze} 
+      options={{ trigger: 'hover' }}>
+      <StyledItem>
+        <Image 
+          src={imageUrl} 
+          onClick={onToDetailPage} 
+          {...props}>
+        </Image>
+      </StyledItem>
+    </ReactFreezeframe>
   );
 };
 
 const StyledItem = styled.div`
+  display: flex;
   width: 152px;
   height: 152px;
   padding: 8px;
+  justify-content: center;
+  box-sizing: border-box;
+  border-radius: 4px;
   overflow: hidden;
-  align-content: center;
+  &:hover img {
+    transform: scale(1.8);        
+    -webkit-transform: scale(1.1);
+    -moz-transform: scale(1.1);  
+    transition: all 0.2s ease-in;
+  }
 `;
 
 ZzalItem.propTypes = {
+  id: PropTypes.string.isRequired,
   imageUrl: PropTypes.string
 };
-
 
 export default ZzalItem;
