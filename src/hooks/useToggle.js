d179d5fc77b5bea428acf07244f8ef5b4561eb57
khj <hongjung.kim.dev@gmail.com>
2021-10-25 17:36:04 +0900

    fix: 이전 커밋의 callback 최적화 과정의 실수로 다시 작성

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -3,7 +3,7 @@ import { useCallback, useState } from 'react';
 const useToggle = (currentState = false) => {
   const [state, setState] = useState(currentState);
 
-  const handleToggle = useCallback(() => setState(state => !state));  
+  const handleToggle = useCallback(() => setState(state => !state), []);  
   
   return [state, handleToggle];
 };
