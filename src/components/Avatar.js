353bcc769df896031b4c6c2d50ad291fdbfd0580
Kim hyunseok <dorrdorr9311@gmail.com>
2021-10-25 18:50:52 +0900

    chore: avatar.js 오타 수정

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -3,16 +3,16 @@ import Image from '@components/base/Image';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 
-const defaultProfileImage = 'https://t1.daumcdn.net/cfile/tistory/2513B53E55DB206927'; 
+
+const DEFAULT_PROFILE_URL = 'https://t1.daumcdn.net/cfile/tistory/2513B53E55DB206927'; 
 
 const StyledImage = styled(Image)`
   outline: 2px solid;
-  cursor: pointer;
   overflow: hidden;
   object-fit: cover;
 `;
 
-const Avatar = ({ src = defaultProfileImage, size, onClick }) => {
+const Avatar = ({ src = DEFAULT_PROFILE_URL, size, onClick }) => {
 
   return (
     <StyledImage
@@ -31,9 +31,6 @@ Avatar.propTypes = {
     PropTypes.string,
     PropTypes.number
   ]),
-  lazy: PropTypes.bool,
-  threshold: PropTypes.number,
-  placeholder: PropTypes.string,
   onClick: PropTypes.func
 };
 
