0e5fc0c9ce1b415b10ef4ba905cbdb790c505452
Alang Kim <rlaangh77@naver.com>
2021-10-27 04:04:28 +0900

    feat: MenuBar 컴포넌트 구현

--- src/domains/MenuBar/index.js
+++ src/domains/MenuBar/index.js
@@ -1,2 +1,33 @@
-export { default as MenuBarContainer } from './MenuBarContainer';
-export { default as MenuBarItem } from './MenuBarItem';
+import React from 'react';
+import { useHistory } from 'react-router';
+import PropTypes from 'prop-types';
+
+import Divider from '@base/Divider';
+import MenuBarContainer from '@domains/MenuBar/MenuBarContainer';
+import MenuBarItem from '@domains/MenuBar/MenuBarItem';
+import colors from '@constants/colors';
+
+const MenuBar = ({ userId, ...props }) => {
+  const history = useHistory();
+
+  return (
+    <MenuBarContainer {...props}>
+      <MenuBarItem onClick={() => history.push(`/user/${userId}`)}>
+                  개인 페이지
+      </MenuBarItem>
+      <MenuBarItem onClick={() => history.push('/editProfile')}>
+                  정보 수정
+      </MenuBarItem>
+      <Divider color={colors.PRIMARY_BACKGROUND} />
+      <MenuBarItem onClick={() => alert('로그아웃!')}>
+                  로그아웃
+      </MenuBarItem>
+    </MenuBarContainer>
+  );
+};
+
+MenuBar.propTypes = {
+  userId: PropTypes.string.isRequired
+};
+
+export default MenuBar;
