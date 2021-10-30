d44ece74ebd15c04166d1f402e9ee0f2509e7671
Kim hyunseok <dorrdorr9311@gmail.com>
2021-10-30 11:19:04 +0900

    feat: 리팩토링한 tab을 zzalFeed에 적용

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { Global, css } from '@emotion/react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
+  //Link
 } from 'react-router-dom';
 
 import Header from '@domains/Header';
@@ -37,7 +37,7 @@ function App() {
         <Header />
         <input type='checkbox' checked={isAuthorized} onChange={handleAuthorized} />
         {isAuthorized ? '로그인 됨' : '로그인 안됨'}
-        <nav>
+        {/* <nav>
           <ul>
             <li>
               <Link to="/">메인페이지</Link>
@@ -52,7 +52,6 @@ function App() {
               <Link to="/search/hongjung">검색결과</Link>
             </li>
             <li>
-              {/* <Link to="/user/1">유저1</Link>  */}
               <Link to="/user/61759164359c4371f68ac707">유저1</Link>
             </li>
             <li>
@@ -62,7 +61,7 @@ function App() {
               <Link to="/upload">업로드</Link>
             </li>
           </ul>
-        </nav>
+        </nav> */}
         <Switch>
           <Route path="/login">
             <LoginPage />
--- src/domains/Zzal/ZzalFeed.js
+++ src/domains/Zzal/ZzalFeed.js
@@ -1,39 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import ZzalList from './ZzalList';
 import Tab from '@domains/Tab';
 import Text from '@base/Text';
-//import useAxios from '@hooks/useAxios';
+import useAxios from '@hooks/useAxios';
 
 
-const ZzalFeed = ({ likeZzals }) => {
-  //const [userPostData, fetchUserPostData] = useAxios(`/posts/author/${userId}`);
+const ZzalFeed = ({ userId, likeZzals }) => {
+  const [userPostData, fetchUserPostData] = useAxios(`/posts/author/${userId}`);
   const likeZzalPosts = likeZzals.map(zzal => zzal.post);
   const likeZzalPostState = {
     isLoading: false,
     value: likeZzalPosts
   };
-  //console.log(userPostData);
-  //const { isLoading, value, error } = userPostData; 
 
-  // if (isLoading) {
-  //   return <div>Loading...</div>; 
-  // }
-  // if (error) {
-  //   return <div>에러발생</div>;
-  // }
-  // if (!value) {
-  //   return <button onClick={fetchUserPostData}>불러오기</button>;
-  // }
+  useEffect(() => {
+    fetchUserPostData();
+  }, []);
+
+  const { isLoading, value, error } = userPostData; 
+
+  if (isLoading) {
+    return <div>Loading...</div>; 
+  }
+  
+  if (error) {
+    return <div>에러발생</div>;
+  }
+  if (!value) {
+    return <button onClick={fetchUserPostData}>불러오기</button>;
+  }
 
   return (
     <>
       <Tab>
-        <Text bold>좋아요</Text>
-        <Text bold>업로드</Text>
+        <Tab.Header>
+          <Tab.Item index={0}><Text bold>좋아요</Text></Tab.Item>  
+          <Tab.Item index={1}><Text bold>업로드</Text></Tab.Item>
+        </Tab.Header>
+        <Tab.Panel>
+          <ZzalList index={0} zzalList={likeZzalPostState} ></ZzalList>
+          <ZzalList index={1} zzalList={userPostData} ></ZzalList>
+        </Tab.Panel>
       </Tab>
-      <ZzalList zzalList={likeZzalPostState}></ZzalList>
+      
     </>
   );
 };
