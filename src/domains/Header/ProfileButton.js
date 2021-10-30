d7e17602be22ec85d8bbc928376ecf568499659f
Alang Kim <rlaangh77@naver.com>
2021-10-30 17:16:05 +0900

    fix: header에서 user 이미지 받아오지 않는 문제 해결

--- src/domains/Header/ProfileButton.js
+++ src/domains/Header/ProfileButton.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import imageSrc from '@assets/no_img.png';
+import noImage from '@assets/no_img.png';
 import useClickAway from '@hooks/useClickAway';
 import useToggle from '@hooks/useToggle';
 import Avatar from '@components/Avatar';
 import MenuBar from '@domains/MenuBar';
 
 
-const ProfileButton = ({ userId, ...props }) => {
+const ProfileButton = ({ myUser, ...props }) => {
   const [showMenuBar, toggleMenuBar] = useToggle(false);
 
   const menuBarRef = useClickAway(() => {
@@ -21,14 +21,14 @@ const ProfileButton = ({ userId, ...props }) => {
 
   return (
     <div ref={menuBarRef} style={{ position: 'relative' }} {...props}>
-      <Avatar src={imageSrc} size={45} onClick={toggleMenuBar} />
-      <MenuBar userId={userId} style={{ display: showMenuBar ? 'flex' : 'none' }} />
+      <Avatar src={myUser?.image || noImage} size={45} onClick={toggleMenuBar} />
+      <MenuBar userId={myUser?._id} style={{ display: showMenuBar ? 'flex' : 'none' }} />
     </div>
   );
 };
 
 ProfileButton.propTypes = {
-  userId: PropTypes.string
+  myUser: PropTypes.object
 };
 
 export default ProfileButton;
