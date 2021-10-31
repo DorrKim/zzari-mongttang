45f61da96ab87749bfa6998058e9ca48f42af1e5
Kim hyunseok <dorrdorr9311@gmail.com>
2021-10-31 14:51:42 +0900

    feat: ConfirmModal 컴포넌트 구현

--- src/stories/domains/NotationModal/AlertModal.stories.js
+++ src/stories/domains/NotationModal/AlertModal.stories.js
@@ -13,8 +13,8 @@ export const Dafault = props => {
     <>
       <button onClick={() => setVisible(true)} >Show Alert Modal</button>
       <AlertModal
-        title='로그인 필요'
-        description='현재 페이지는 로그인이 필요한 서비스입니다.'
+        title='국민의 재산권'
+        description='군인 또는 군무원이 아닌 국민은 대한민국의 영역안에서는 중대한 군사상 기밀·초병·초소·유독음식물공급·포로·군용물에 관한 죄중 법률이 정한 경우와 비상계엄이 선포된 경우를 제외하고는 군사법원의 재판을 받지 아니한다'
         visible={visible}
         handleClose={() => setVisible(false)} 
         {...props}>
