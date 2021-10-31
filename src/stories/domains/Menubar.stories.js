2eb932233525aa1da447fae52996d12bdf8a628f
Kim hyunseok <dorrdorr9311@gmail.com>
2021-10-31 22:11:20 +0900

    chore: 잘못된 주석처리 제거

--- src/stories/domains/Menubar.stories.js
+++ src/stories/domains/Menubar.stories.js
@@ -1,25 +1,25 @@
-// import React from 'react';
+import React from 'react';
 
-// import { MenuBarContainer, MenuBarItem } from '@domains/MenuBar';
-// import Divider from '@base/Divider';
-// import colors from '@constants/colors';
+import { MenuBarContainer, MenuBarItem } from '@domains/MenuBar';
+import Divider from '@base/Divider';
+import colors from '@constants/colors';
 
 
-// export default {
-//   title: 'Domains/Menubar'
-// };
+export default {
+  title: 'Domains/Menubar'
+};
 
-// export const Default = () => (
-//   <MenuBarContainer>
-//     <MenuBarItem onClick={() => alert('to 개인 페이지!')}>
-//       개인 페이지
-//     </MenuBarItem>
-//     <MenuBarItem onClick={() => alert('to 정보수정 페이지!')}>
-//       정보 수정
-//     </MenuBarItem>
-//     <Divider color={colors.PRIMARY_BACKGROUND} />
-//     <MenuBarItem onClick={() => alert('로그아웃!')}>
-//       로그아웃
-//     </MenuBarItem>
+export const Default = () => (
+  <MenuBarContainer>
+    <MenuBarItem onClick={() => alert('to 개인 페이지!')}>
+      개인 페이지
+    </MenuBarItem>
+    <MenuBarItem onClick={() => alert('to 정보수정 페이지!')}>
+      정보 수정
+    </MenuBarItem>
+    <Divider color={colors.PRIMARY_BACKGROUND} />
+    <MenuBarItem onClick={() => alert('로그아웃!')}>
+      로그아웃
+    </MenuBarItem>
 
-//   </MenuBarContainer>);
+  </MenuBarContainer>);
