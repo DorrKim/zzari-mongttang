2d21ee0aa3390424ff0e8403b4a64d1d4a942b64
Kim hyunseok <dorrdorr9311@gmail.com>
2021-10-31 18:38:41 +0900

    feat: comment 작성 경과 시간을 알려주는 parseCommentCreateDate 함수 구현

--- src/domains/Comment/CommentItem.js
+++ src/domains/Comment/CommentItem.js
@@ -3,19 +3,48 @@ import PropTypes from 'prop-types';
 import Avatar from '@components/Avatar';
 import styled from '@emotion/styled';
 import Text from '@base/Text';
+import colors from '@constants/colors';
+import Flex from '@base/Flex';
+
+
+const parseCommentCreateDate = createDate => {
+  const systemDate = new Date(Date.parse(createDate));
+  const newDate = new Date();
+
+  const diffSeconds = Math.floor((newDate - systemDate) / 1000);
+  
+  if (diffSeconds < 60) {
+    return '방금 전';
+  }
+  if (diffSeconds < 60 * 60) {
+    return `${Math.round(diffSeconds / 60)}분 전`;
+  }
+  if (diffSeconds < 60 * 90) {
+    return '1시간 전';
+  }
+  if (diffSeconds < 60 * 60 * 24) {
+    return `${Math.round(diffSeconds / (60 * 60))}시간 전`;
+  }
+  if (diffSeconds < 60 * 60 * 24 * 1.5) {
+    return '1일 전';
+  }
+  if (diffSeconds < 60 * 60 * 24 * 7) {
+    return `${Math.round(diffSeconds / (60 * 60 * 24))}일 전`;
+  }
+};
 
 const CommentItemWrapper = styled.div`
   display: flex;
   align-items: center;
-  width: 328px;
-  height: 80px;
+  width: 320px;
+ 
   margin-top: 16px;
   background-color: #FBF7F2;
   border-radius: .5rem;
 `;
 
 const CommentInfo = styled.div`
-  height: 100%;
+  flex: 1 1;
   padding: 12px 8px;
   display: flex;
   flex-direction: column;
@@ -23,12 +52,16 @@ const CommentInfo = styled.div`
 `;
 
 const CommentItem = ({ comment }) => {
-    
+  
   return (
     <CommentItemWrapper>
       <Avatar src={comment.author.image} size='64px' style={{ margin: '8px 8px 8px 12px' }}/>
       <CommentInfo>
-        <Text bold>{comment.author.fullName}</Text>
+        <Flex alignItems='center'>
+          <Text bold>{comment.author.fullName}</Text>
+          <Text block size="sm" bold color={colors.TEXT_SUBTLE} style={{ marginLeft: '8px',
+            maxWidth: '200px' }}>{parseCommentCreateDate(comment.createdAt)}</Text>
+        </Flex>
         <Text style={{ marginTop: '8px' }}>{comment.comment}</Text>
       </CommentInfo>
     </CommentItemWrapper>
