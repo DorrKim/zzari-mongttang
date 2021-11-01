7f5e4b1ba54d6fd0d16393bc2b5376707ccc0e50
Alang Kim <rlaangh77@naver.com>
2021-11-01 20:55:47 +0900

    fix: favorite 기능 zzalItem 컴포넌트에서 Favorite 컴포넌트로 이동

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -1,22 +1,92 @@
-import React from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 
 import Icon from '@components/base/Icon';
 import Number from '@components/Number';
 import colors from '@constants/colors';
+import useToggle from '@hooks/useToggle';
+import { useAuthorization } from '@context/AuthorizationProvider';
+import { useHistory } from 'react-router';
+import useAxios from '@hooks/useAxios';
 
 const FavoriteContainer = styled.div`
   display: flex;
   width: 40px;
   justify-content: flex-end;
   opacity: 1;
-  &:hover :nth-child(1){
+  &:hover :nth-of-type(1){
     color: ${colors.ACCENT};
   }
 `;
 
-const Favorite = ({ number, onClick, isToggled, ...props }) => {
+const Favorite = ({ likes, postId, ...props }) => {
+  const history = useHistory();
+  const { authState: { isAuthorized, myUser, authToken }} = useAuthorization();
+  const headers = useMemo(() => ({ Authorization: `Bearer ${authToken}` }), [authToken]);
+
+  const [isToggled, toggle] = useToggle(likes.some(({ user }) => user === myUser._id));
+  const [likeCount, setLikeCount] = useState(likes.length);
+  const [myLikesId, setMyLikesId] = useState(likes.find(({ user }) => user === myUser._id)?._id);
+
+  const [postFavoriteAPIState, postFavorite] = useAxios('/likes/create', {
+    method: 'post'
+  });
+  const [deleteFavoriteAPIState, deleteFavorite] = useAxios('/likes/delete', {
+    method: 'delete'
+  });
+
+  const handleClickFavorite = useCallback(() => {
+    if (!isAuthorized) {
+      confirm('로그인이 필요한 기능입니다! 로그인 페이지로 이동하시겠습니까?') && history.push('/login');
+
+      return;
+    }
+    isToggled
+      ? (
+        deleteFavorite({
+          headers,
+          data: {
+            id: myLikesId
+          }
+        }), 
+        setLikeCount(likeCount => likeCount - 1),
+        setMyLikesId(null)
+      )
+      : (
+        postFavorite({
+          headers,
+          data: {
+            postId
+          }
+        }), 
+        setLikeCount(likeCount => likeCount + 1)
+      );
+    toggle();
+  }, [isAuthorized, isToggled, headers, myLikesId, postId, deleteFavorite, postFavorite]);
+
+
+  useEffect(() => {
+    const { error } = deleteFavoriteAPIState;
+    if (error) {
+      console.error(error);
+
+      return;
+    }
+  }, [deleteFavoriteAPIState]);
+
+  useEffect(() => {
+    const { error, value } = postFavoriteAPIState;
+    if (error) {
+      console.error(error);
+
+      return;
+    }
+
+    value && setMyLikesId(value._id);
+  }, [postFavoriteAPIState]);
+
+
   const toggledStyle = {
     color: colors.ACCENT,
     opacity: 1,
@@ -38,21 +108,19 @@ const Favorite = ({ number, onClick, isToggled, ...props }) => {
       <FavoriteContainer {...props}>
         <Icon 
           name={isToggled ? 'filledHeart' : 'heart'} 
-          onClick={onClick} 
+          onClick={handleClickFavorite} 
           style={isToggled ? { ...toggledStyle } : { ...unToggledStyle }}
         >
         </Icon>
-        <Number value={number}></Number>
+        <Number value={likeCount}></Number>
       </FavoriteContainer>
     </>
   );
 };
 
 Favorite.propTypes = {
-  number: PropTypes.oneOfType([
-    PropTypes.number,
-    PropTypes.string
-  ]),
+  likes: PropTypes.array,
+  postId: PropTypes.string,
   onClick: PropTypes.func,
   isToggled: PropTypes.bool
 };
