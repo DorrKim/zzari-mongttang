a5422ed144cb4718a28392718c5613e2c4b37c89
Alang Kim <rlaangh77@naver.com>
2021-11-04 15:29:41 +0900

    hotfix: alertModal 미반영 부분 적용

--- src/domains/Header/index.js
+++ src/domains/Header/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useHistory } from 'react-router';
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
@@ -9,16 +9,18 @@ import Logo from '@components/Logo';
 import colors from '@constants/colors';
 import { useAuthorization } from '@context/AuthorizationProvider';
 import ProfileButton from './ProfileButton';
+import LoginConfirmModal from '@domains/NotationModal/LoginConfirmModal';
 
 const Header = ({ ...props }) => {
   const history = useHistory();
+  const [isShowLoginConfirm, setIsShowLoginConfirm] = useState(false);
   const { authState } = useAuthorization();
   const { isAuthorized } = authState;
   
   const handleToUploadPage = useCallback(() => {
     if (!isAuthorized) {
-      confirm('해당 기능을 이용하기 위해서 로그인이 필요합니다. 로그인 페이지로 이동하시겠습니까?') && history.push('/login');
-
+      setIsShowLoginConfirm(true);
+      
       return;
     }
 
@@ -31,24 +33,31 @@ const Header = ({ ...props }) => {
 
 
   return (
-    <HeaderStyled {...props}>
-      <Logo link style={{ marginRight: 'auto' }} />
-      <HeaderButton 
-        backgroundColor={colors.PRIMARY_BACKGROUND}
-        onClick={handleToUploadPage}
-      > 
-        <Text style={{ whiteSpace: 'nowrap' }} bold color={colors.PRIMARY}>글 작성</Text> 
-      </HeaderButton>
-      {isAuthorized
-        ? <ProfileButton myUser={authState.myUser} />
-        : <HeaderButton 
-          backgroundColor={colors.PRIMARY}
-          onClick={handleToLoginPage}
-        >
-          <Text style={{ whiteSpace: 'nowrap' }} bold color={colors.PRIMARY_BACKGROUND}>로그인</Text>
-        </HeaderButton>  
-      }
-    </HeaderStyled>
+    <>
+      <HeaderStyled {...props}>
+        <Logo link style={{ marginRight: 'auto' }} />
+        <HeaderButton 
+          backgroundColor={colors.PRIMARY_BACKGROUND}
+          onClick={handleToUploadPage}
+        > 
+          <Text style={{ whiteSpace: 'nowrap' }} bold color={colors.PRIMARY}>글 작성</Text> 
+        </HeaderButton>
+        {isAuthorized
+          ? <ProfileButton myUser={authState.myUser} />
+          : <HeaderButton 
+            backgroundColor={colors.PRIMARY}
+            onClick={handleToLoginPage}
+          >
+            <Text style={{ whiteSpace: 'nowrap' }} bold color={colors.PRIMARY_BACKGROUND}>로그인</Text>
+          </HeaderButton>  
+        }
+      </HeaderStyled>
+      <LoginConfirmModal
+        visible={isShowLoginConfirm}
+        handleClickConfirm={() => history.push('/login')}
+        handleClickCancel={() => setIsShowLoginConfirm(false)}
+      />
+    </>
   );
 };
 
